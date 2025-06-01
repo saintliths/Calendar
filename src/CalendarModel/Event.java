9e6ddf47a136bd901f86730a5feaaa8e9d83f895
LeiVanessa <lei.va@northeastern.edu>
2025-06-01 14:37:40 -0400

    Added Event fields, and default constructor + constructor with arguments

--- src/CalendarModel/Event.java
+++ src/CalendarModel/Event.java
@@ -5,11 +5,34 @@ import java.time.LocalDateTime;
 public class Event {
 
   private final String subject;
-  private final LocalDateTime StartDateAndTime;
-  private final LocalDateTime EndDateAndTime;
+  private final LocalDateTime startDateTime;
+  private final LocalDateTime endDateTime;
   private final String description;
   private final String location;
-  private final boolean visibility; // private/public
-  // missing event ID
+  private final boolean isPrivate;
+  private final boolean partOfSeries;
+
+  private Event(String subject, LocalDateTime start, LocalDateTime end,
+               String description, String location, boolean isPrivate, boolean partOfSeries) {
+    this.subject = subject;
+    this.startDateTime = start;
+    this.endDateTime = end;
+    this.description = description;
+    this.location = location;
+    this.isPrivate = isPrivate;
+    this.partOfSeries = partOfSeries;
+  }
+
+  // default constructor
+  public Event(String subject, LocalDateTime date, String description,
+               String location, boolean isPrivate, boolean partOfSeries) {
+    this.subject = subject;
+    this.startDateTime = date.withHour(8).withMinute(0);
+    this.endDateTime = date.withHour(17).withMinute(0);
+    this.description = description;
+    this.location = location;
+    this.isPrivate = isPrivate;
+    this.partOfSeries = partOfSeries;
+  }
 
 }
