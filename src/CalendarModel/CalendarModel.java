a728ec8e328c0d819a2d79dcc8fa94faeb7cc37d
nesslei <lei.va@northeastern.edu>
2025-06-01 13:42:32 -0400

    Added EventsByDate into CalenderModel

--- src/CalendarModel/CalendarModel.java
+++ src/CalendarModel/CalendarModel.java
@@ -1,4 +1,11 @@
 package CalendarModel;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
 public class CalendarModel implements IModel {
+
+  Map<LocalDateTime, List<Event>> eventsByDate;
+
 }
