4ab3362816aad0a457115d4b66414a109926961c
Jennifer Nguyen <nguyen.jennifer@northeastern.edu>
2025-06-02 09:49:26 -0400

    Added main method

---
+++ src/CalendarExampleUI.java
@@ -0,0 +1,15 @@
+import CalendarController.CalendarController;
+import CalendarController.IController;
+import CalendarModel.CalendarModel;
+import CalendarModel.IModel;
+import CalendarView.CalendarView;
+import CalendarView.IView;
+
+public class CalendarExampleUI {
+  public static void main(String []args) {
+    IModel model = new CalendarModel();
+    IView view = new CalendarView(System.out);
+    IController controller = new CalendarController(model, System.in,view);
+    controller.go();
+  }
+}
