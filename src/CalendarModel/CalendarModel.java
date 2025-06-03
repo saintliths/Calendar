ea0e745b1be42287af5f92b8f1efa819eb3734d9
LeiVanessa <lei.va@northeastern.edu>
2025-06-03 15:18:59 -0400

    Update CalendarModel.java

--- src/CalendarModel/CalendarModel.java
+++ src/CalendarModel/CalendarModel.java
@@ -29,6 +29,43 @@ public class CalendarModel implements IModel {
   public void setString(String i) {
     input = i;
   }
+<<<<<<< Updated upstream
+=======
+
+
+  @Override
+  public Event createEvent(String input) {
+    String[] arg = input.split(" ");
+    String[] start = arg[4].split("T");
+    LocalDate startDate = LocalDate.parse(start[0]);
+    LocalTime startTime = LocalTime.parse(start[1]);
+
+    EventBuilder e = new EventBuilder(arg[2], startDate, startTime);
+
+    if (arg.length == 7) {
+      String[] end = arg[6].split("T");
+      LocalDate endDate = LocalDate.parse(end[0]);
+      LocalTime endTime = LocalTime.parse(end[1]);
+      e.endDate(endDate).endTime(endTime);
+    }
+
+
+    if (eventsByDate.containsKey(startDate)) {
+      eventsByDate.get(startDate).add(e.build());
+    } else {
+      List<Event> newStartDate = new ArrayList<>();
+      newStartDate.add(e.build());
+      eventsByDate.put(startDate, newStartDate);
+    }
+    return e.build();
+  }
+
+  
+  @Override
+  public EventSeries createEventSeries(String input) {
+
+  }
+>>>>>>> Stashed changes
 
 
   @Override
